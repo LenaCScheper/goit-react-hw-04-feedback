466f757419d6f709ca23a242e7634cfadfb212cc
LenaCScheper <ven.lenysja@ukr.net>
2022-04-03 14:37:59 +0300

    скопировала предыдущию дз

---
+++ build/precache-manifest.63ee74fda06824a9792a4dcb2032c0e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206e0c91c0479e261713156d83993062",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "9a029bbf548fa1b931cd",
+    "url": "/goit-react-hw-02-feedback/static/js/2.28bccf51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.28bccf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0533064a044e97602947",
+    "url": "/goit-react-hw-02-feedback/static/js/main.e4ffb52b.chunk.js"
+  },
+  {
+    "revision": "67fc603d4fe28264fbd4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.56a9b6a1.js"
+  }
+]);
